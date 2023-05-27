f518f84bd326c0b9f025128c518e0f8a087e4801
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2023-05-27 19:59:59 +0300

    update

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,18 +1,32 @@
 
 
 export const ContactList = ({contacts, onDeletContacts}) => {
+    // return (
+    //     <ul>
+    //     {contacts.map(({name, number, id}) => {
+    //         <li key={id}>
+    //            <p>{name}: {number}</p>
+    //            <button 
+    //            type="button"
+    //            onClick={() => onDeletContacts(id)}
+    //            >Delete
+    //            </button>
+    //         </li>
+    //     })}
+    //     </ul>
+    // )
     return (
         <ul>
-        {contacts.map(({name, number, id}) => {
+          {contacts.map(({ name, number, id }) => (
             <li key={id}>
-               <p>{name}: {number}</p>
-               <button 
-               type="button"
-               onClick={() => onDeletContacts(id)}
-               >Delete
-               </button>
+              <p>
+                {name}: {number}
+              </p>
+              <button type="button" onClick={() => onDeletContacts(id)}>
+                delete
+              </button>
             </li>
-        })}
+          ))}
         </ul>
-    )
+      );
 }
